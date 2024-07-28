6bbc258fe31db9404b0752a19761176a3ddc750a
Kieran David Evans <keyz182@gmail.com>
2024-07-28 10:41:45 +0100

    Cookie cutter initial commit

---
+++ 1.5/Source/Inbetween/InbetweenDefOf.cs
@@ -0,0 +1,14 @@
+﻿using RimWorld;
+using Verse;
+
+namespace Inbetween;
+
+[DefOf]
+public static class InbetweenDefOf
+{
+    // Remember to annotate any Defs that require a DLC as needed e.g.
+    // [MayRequireBiotech]
+    // public static GeneDef YourPrefix_YourGeneDefName;
+    
+    static InbetweenDefOf() => DefOfHelper.EnsureInitializedInCtor(typeof(InbetweenDefOf));
+}
---
+++ 1.5/Source/Inbetween/InbetweenMod.cs
@@ -0,0 +1,34 @@
+﻿using Verse;
+using UnityEngine;
+using HarmonyLib;
+
+namespace Inbetween;
+
+public class InbetweenMod : Mod
+{
+    public static Settings settings;
+
+    public InbetweenMod(ModContentPack content) : base(content)
+    {
+        Log.Message("Hello world from Inbetween");
+
+        // initialize settings
+        settings = GetSettings<Settings>();
+#if DEBUG
+        Harmony.DEBUG = true;
+#endif
+        Harmony harmony = new Harmony("keyz182.rimworld.Inbetween.main");	
+        harmony.PatchAll();
+    }
+
+    public override void DoSettingsWindowContents(Rect inRect)
+    {
+        base.DoSettingsWindowContents(inRect);
+        settings.DoWindowContents(inRect);
+    }
+
+    public override string SettingsCategory()
+    {
+        return "Inbetween_SettingsCategory".Translate();
+    }
+}
